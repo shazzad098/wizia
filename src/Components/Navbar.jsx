e6a1190edcb9b5c68fb33e7d29e4018cb7533528
Shazzad Prince <shazzad920@gmail.com>
2025-03-24 22:13:47 +0600

    completed navbar for the lg screen devices

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+} from "@heroui/react";
+
+
+function NavbarArea() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen} className="bg-[#002228]">
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <img src="Logo.png" alt="" />
+        </NavbarBrand>
+
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link className="text-white" href="#">
+            About Us
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="text-white" href="#">
+            Pricing
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="text-white" color="foreground" href="#">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="text-white" color="foreground" href="#">
+            Solutions
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Button className="text-black rounded-full bg-main" as={Link} href="#" variant="flat">
+            Book a Demo
+          </Button>
+
+        </NavbarItem>
+        <NavbarItem>
+          <Button className="text-white rounded-full bg-transparent border" as={Link} href="#" variant="flat">
+            Contact Us
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
+
+export default NavbarArea
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+const { heroui } = require("@heroui/react");
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        main: "#0FF1F6"
+      }
+    },
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+}
