967d5a50c5d7db8df0d45ae02c61d760f9b30974
Shazzad Prince <shazzad920@gmail.com>
2025-04-05 22:09:09 +0600

    main

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import MarketingBanner from './Components/MarketingBanner'
 import TestimonialSlider from './Components/TestimonialSlider'
 import StateSection from './Components/StateSection'
 import Footer from './Components/Footer'
-import CTASection from './Components/Ctasection'
+import CTASection from './Components/CTASection'
 
 
 
